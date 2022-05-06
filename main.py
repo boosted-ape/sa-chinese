49bfb704ce8931bdbd8e5eeb937fb932504910d7
Boon Jhee <wbjhee@gmail.com>
2022-05-06 15:44:49 +0700

    initial commit

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras import regularizers
+import jieba.posseg as pseg
+import jieba
+from keras.preprocessing.text import text_to_word_sequence
+
+string = "（早报讯）我国对所有完成冠病疫苗接种者重开边境一个月来，樟宜机场接待了更多旅客，4月底的航空乘客量进一步增加至疫前水平的近40％，相比3月的约18％增加了超过一倍。乘客量下来几个月还会继续增长，相信今年内可如预期恢复到疫前水平的至少50%。交通部长易华仁今午（5月4日）在樟宜机场集团的“樟宜航空大奖”颁奖礼上致辞时指出，我国不仅要恢复以前的航空衔接，也要进一步扩大航空网络、增加航班的密集度，以及与航空公司建立新的伙伴关系。我国4月起实行简化的疫苗接种者旅游框架，所有已完成冠病疫苗接种的旅客，不论来自哪个国家或地区都可入境新加坡，4月26日起也不再须要接受行前冠病检测或隔离。"
+max_words = 5000
+max_len = 200
+
+def analyzer(string):
+    words = pseg.cut(string)
+    word_sequence = []
+
+    for w in words:
+        if(w.flag == "x"):
+            continue
+        word_sequence.append(w.word)
+    
+    return word_sequence
+
+tokenizer = Tokenizer(num_words=max_words)
+word_sequence = analyzer(string)
+tokenizer.fit_on_texts(word_sequence)
+sequences = tokenizer.texts_to_sequences(word_sequence)
+tweets = pad_sequences(sequences, maxlen=max_len)
+print(tweets)
+
 
