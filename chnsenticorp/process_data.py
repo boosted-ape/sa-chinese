7d87f446c5a57d7542981f75dd6511b19aa2b859
Boon Jhee <wbjhee@gmail.com>
2022-05-11 01:41:29 +0700

    processed data

---
+++ chnsenticorp/process_data.py
@@ -0,0 +1,14 @@
+original_file = open("test/part.txt", "r")
+f1 = open("test/1.txt", "a")
+f0 = open("test/0.txt", "a")
+Lines = original_file.readlines()
+
+for line in Lines:
+    if (line[-2] == "0"):
+        f0.write(line[:-2]+"\n")
+    else:
+        f1.write(line[:-2]+"\n")
+
+original_file.close()
+f1.close()
+f0.close()
