bd768fd20c14c871298006f6ed34e2c15538eda3
Boon Jhee <wbjhee@gmail.com>
2022-05-14 19:49:25 +0700

    filter punctuation

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import collections
 import numpy
 
 
-string = "（早报讯）我国对所有完成冠病疫苗接种者重开边境一个月来，樟宜机场接待了更多旅客，4月底的航空乘客量进一步增加至疫前水平的近40％，相比3月的约18％增加了超过一倍。乘客量下来几个月还会继续增长，相信今年内可如预期恢复到疫前水平的至少50%。交通部长易华仁今午（5月4日）在樟宜机场集团的“樟宜航空大奖”颁奖礼上致辞时指出，我国不仅要恢复以前的航空衔接，也要进一步扩大航空网络、增加航班的密集度，以及与航空公司建立新的伙伴关系。我国4月起实行简化的疫苗接种者旅游框架，所有已完成冠病疫苗接种的旅客，不论来自哪个国家或地区都可入境新加坡，4月26日起也不再须要接受行前冠病检测或隔离。"
+example_sentence = "（早报讯）我国对所有完成冠病疫苗接种者重开边境一个月来，樟宜机场接待了更多旅客，4月底的航空乘客量进一步增加至疫前水平的近40％，相比3月的约18％增加了超过一倍。乘客量下来几个月还会继续增长，相信今年内可如预期恢复到疫前水平的至少50%。交通部长易华仁今午（5月4日）在樟宜机场集团的“樟宜航空大奖”颁奖礼上致辞时指出，我国不仅要恢复以前的航空衔接，也要进一步扩大航空网络、增加航班的密集度，以及与航空公司建立新的伙伴关系。我国4月起实行简化的疫苗接种者旅游框架，所有已完成冠病疫苗接种的旅客，不论来自哪个国家或地区都可入境新加坡，4月26日起也不再须要接受行前冠病检测或隔离。"
 VOCAB_SIZE = 50000
 max_len = 200
 
@@ -32,12 +32,17 @@ segmenter = HubModuleTokenizer(MODEL_HANDLE)
 def labeler(example, index):
     return example, tf.cast(index, tf.int64)
 
+
 def configure_dataset(dataset):
   return dataset.cache().prefetch(buffer_size=AUTOTUNE)
 
 
+punc = "！？｡。＂＃＄％＆＇（）＊＋，－／：；＜＝＞＠［＼］＾＿｀｛｜｝～｟｠｢｣､、〃》「」『』【】〔〕〖〗〘〙〚〛〜〝〞〟〰〾〿–—‘’‛“”„‟…‧﹏."
 
 
+
+train_dataset_0 = train_dataset_0.map(lambda x: tf.strings.regex_replace(x, "[%s]+" %punc, " "))
+train_dataset_1 = train_dataset_1.map(lambda x: tf.strings.regex_replace(x, "[%s]+" %punc, " "))
 
 train_dataset_0 = train_dataset_0.map(lambda x: segmenter.tokenize(x))
 train_dataset_1 = train_dataset_1.map(lambda x: segmenter.tokenize(x))
@@ -68,7 +73,8 @@ vocab = vocab[:VOCAB_SIZE]
 vocab_size = len(vocab)
 
 print("Vocab size: ", vocab_size)
-print("First five vocab entries:", vocab[:5])
+for i in range(5):
+    print(vocab[i].decode("utf-8"))
 
 
 model = Sequential()
