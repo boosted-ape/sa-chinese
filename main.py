c8d8bc4ab62b915ffe22bc0297c62cb12523de93
Boon Jhee <wbjhee@gmail.com>
2022-05-17 23:05:35 +0700

    trained network

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from keras.models import Sequential
 from keras import backend as K
 from keras.callbacks import ModelCheckpoint
 from keras import regularizers
-from matplotlib import collections
+import matplotlib.pyplot as plt
 from tensorflow_text import HubModuleTokenizer
 import tensorflow_text as tf_text
 import tensorflow as tf
@@ -13,7 +13,7 @@ import numpy
 
 example_sentence = "（早报讯）我国对所有完成冠病疫苗接种者重开边境一个月来，樟宜机场接待了更多旅客，4月底的航空乘客量进一步增加至疫前水平的近40％，相比3月的约18％增加了超过一倍。乘客量下来几个月还会继续增长，相信今年内可如预期恢复到疫前水平的至少50%。交通部长易华仁今午（5月4日）在樟宜机场集团的“樟宜航空大奖”颁奖礼上致辞时指出，我国不仅要恢复以前的航空衔接，也要进一步扩大航空网络、增加航班的密集度，以及与航空公司建立新的伙伴关系。我国4月起实行简化的疫苗接种者旅游框架，所有已完成冠病疫苗接种的旅客，不论来自哪个国家或地区都可入境新加坡，4月26日起也不再须要接受行前冠病检测或隔离。"
 VOCAB_SIZE = 50000
-max_len = 200
+MAX_SEQUENCE_LENGTH = 250
 
 BUFFER_SIZE = 50000
 BATCH_SIZE = 64
@@ -44,6 +44,10 @@ def configure_dataset(dataset):
 
 
 punc = "！？｡。＂＃＄％＆＇（）＊＋，－／：；＜＝＞＠［＼］＾＿｀｛｜｝～｟｠｢｣､、〃》「」『』【】〔〕〖〗〘〙〚〛〜〝〞〟〰〾〿–—‘’‛“”„‟…‧﹏.《"
+file = open("stopword.txt")
+stopwords = ''.join(file.read().splitlines())
+file.close()
+punc = punc + stopwords
 
 
 train_dataset_0 = train_dataset_0.map(
@@ -96,34 +100,66 @@ init = tf.lookup.KeyValueTensorInitializer(
 
 vocab_size += 2
 
+print(vocab_size)
+
 num_oov_buckets = 1
 vocab_table = tf.lookup.StaticVocabularyTable(init, num_oov_buckets)
+
 
 def preprocess_text(text, label):
     tokenized = segmenter.tokenize(text)
     vectorized = vocab_table.lookup(tokenized)
     return vectorized, label
 
-all_encoded_data = all_labeled_data.map(preprocess_text)
-test_encoded_data = test_labeled_data_sets.map(preprocess_text)
+preprocess_layer = layers.TextVectorization(
+    max_tokens=vocab_size,
+    split=segmenter.tokenize,
+    output_mode='int',
+    output_sequence_length=MAX_SEQUENCE_LENGTH)
 
-train_data = all_encoded_data.padded_batch(BATCH_SIZE)
-test_data = test_encoded_data.padded_batch(BATCH_SIZE)
+preprocess_layer.set_vocabulary(vocab)
+
+
+
+#all_encoded_data = all_labeled_data.map(preprocess_text)
+#test_encoded_data = test_labeled_data_sets.map(preprocess_text)
+
+train_data = all_labeled_data.padded_batch(BATCH_SIZE)
+test_data = test_labeled_data_sets.padded_batch(BATCH_SIZE)
 
 train_data = configure_dataset(train_data)
 test_data = configure_dataset(test_data)
 
+test_ds = test_labeled_data_sets.take(832).batch(BATCH_SIZE)
+test_ds = configure_dataset(test_ds)
 
-model = Sequential()
-model.add(layers.Embedding(VOCAB_SIZE, 20))
-model.add(layers.LSTM(15, dropout=0.5))
-model.add(layers.Dense(2, activation='relu'))
 
-model.compile(optimizer='adam',
-              loss='binary_crossentropy', metrics=['accuracy'])
+model = Sequential([
+    tf.keras.layers.Embedding(
+        input_dim=vocab_size,
+        output_dim=64,
+        # Use masking to handle the variable sequence lengths
+        mask_zero=True),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(1)
+])
 
-checkpoint = ModelCheckpoint("model.hdf5", monitor='val_accuracy', verbose=1,
-                             save_best_only=True, mode='auto', period=1, save_weights_only=False)
+export_model = Sequential([preprocess_layer, model])
 
-history = model.fit(train_data, epochs=3, validation_data=(
-   test_data))
+export_model.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+              optimizer=tf.keras.optimizers.Adam(1e-4),
+              metrics=['accuracy'])
+
+
+checkpoint = ModelCheckpoint("checkpoint/check.ckpt", monitor='val_accuracy', verbose=1,
+                             save_best_only=True, mode='auto', period=1, save_weights_only=True)
+
+history = export_model.fit(train_data, epochs=11, validation_data=(
+    test_data), validation_steps=10, callbacks=checkpoint)
+
+loss, accuracy = export_model.evaluate(test_ds)
+
+print("Loss: ", loss)
+print("Accuracy: {:2.2%}".format(accuracy))
+
